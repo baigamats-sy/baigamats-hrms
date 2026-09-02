f80f17d7863ad3b5f82d4f475625812cd187f600
baigamats-sy <baigamatsinquiries@gmail.com>
2026-09-02 16:23:23 +0300

    Create PeopleSpace.tsx

---
+++ app/PeopleSpace.tsx
@@ -0,0 +1,8 @@
+   // Find this line:
+   import Employees from './pages/Employees'
+   
+   // Change to:
+   import PeopleSpace from './pages/PeopleSpace'
+   
+   // And where it says <Employees />
+   // Change to <PeopleSpace />
